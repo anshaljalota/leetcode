1bae1471d8d30a69b12334780f4f5a71b1113157
anshaljalota <65674540+anshaljalota@users.noreply.github.com>
2020-05-29 19:24:20 +0530

    Update 20. Construct Binary Search Tree from Preorder Traversal.cpp

--- april_leetcoding_challenge/20. Construct Binary Search Tree from Preorder Traversal.cpp
+++ april_leetcoding_challenge/20. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -5,3 +5,37 @@ Return the root node of a binary search tree that matches the given preorder tra
 
 It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int pre_idx;
+    unordered_map<int,int> um;
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int n = preorder.size();
+        vector<int> inorder = preorder;
+        sort(inorder.begin(),inorder.end());
+        for(int i=0;i<n;i++)
+            um[inorder[i]]=i;
+        pre_idx=0;
+        TreeNode * root = util(preorder,0,n-1);
+        return root;
+    }
+    TreeNode *util(vector<int>& preorder, int start, int end){
+        if(start>end)
+            return NULL;
+        TreeNode *root = (TreeNode*) new TreeNode(preorder[pre_idx]);
+        int idx = um[preorder[pre_idx]];
+        pre_idx++;
+        root->left = util(preorder,start,idx-1);
+        root->right = util(preorder,idx+1,end);
+        return root;
+    }
+};
