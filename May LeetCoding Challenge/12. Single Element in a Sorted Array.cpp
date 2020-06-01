0ec7bf3c737ad6900220d3a07bdef64e9ac8b4c8
anshaljalota <65674540+anshaljalota@users.noreply.github.com>
2020-06-01 11:43:15 +0530

    Create 12. Single Element in a Sorted Array.cpp

---
+++ May LeetCoding Challenge/12. Single Element in a Sorted Array.cpp
@@ -0,0 +1,19 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+Follow up: Your solution should run in O(log n) time and O(1) space.
+*/
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int range = nums.size();
+        if(range==0)
+            return 0;
+        int res=0;
+        for(int i =0;i<range;i++){
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+};
